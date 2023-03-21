9d3a40954374a28b04adfdc367167a140f7752c3
Philipp Gallaschik <p,gallaschik@gmail.com>
2023-03-21 13:40:59 +0100

    modfied templates

---
+++ README.md
@@ -0,0 +1,11 @@
+This is the README File for my planets website.
+
+It includes some basic html sites for the first planets of the solar system.
+
+The main page displays a list with links of them, while the subpages display a picture and some basic basic information about them. 
+
+The while site is a Flask application and uses jinja2. 
+
+The earth site is a dynamic page which changes based on the URL.
+
+
--- app.py
+++ app.py
@@ -3,14 +3,18 @@ from random import randint
 
 app = Flask(__name__)
 
-cookies_data = {
+planets_data = {
   'mercury' : {'name': 'Mercury', 'link': 'mercury.html'},
   'venus' : {'name': 'Venus', 'link': 'venus.html'},
   'earth' : {'name': 'Earth', 'link': 'earth.html'},
 }
 
 @app.route('/')
-def cookies():
-  return render_template('planets.html', cookies=cookies_data)
+def planets():
+  return render_template('planets.html', planets = planets_data)
+
+@app.route('/<slug>')
+def slugplanets(slug):
+  return render_template(f"{slug}.html", name = planets_data[slug]['name'], pic = f"{slug}.jpg", planets = planets_data)
 
 app.run()
